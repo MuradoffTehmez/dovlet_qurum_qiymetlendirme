60b1e6d28e7f6379507bef9476b86bfd40293c1f
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-10 19:15:07 +0400

    Add Jazzmin integration and customize admin UI settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,3 +137,62 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+# config/settings.py faylının sonuna əlavə edin
+
+JAZZMIN_SETTINGS = {
+    # Saytın başlığı (brauzer tabında görünür)
+    "site_title": "Qiymətləndirmə Admin",
+
+    # Login səhifəsində və navbar-da görünən başlıq
+    "site_header": "Qiymətləndirmə Sistemi",
+
+    # Saytın brendi (loqo yerinə mətn)
+    "site_brand": "360° Dəyərləndirmə",
+
+    # Loqo (əgər varsa, static qovluğuna qoyulmalıdır)
+    # "site_logo": "images/logo.png",
+
+    # Login səhifəsindəki qarşılama mətni
+    "welcome_sign": "Qiymətləndirmə Sisteminə Xoş Gəlmisiniz",
+
+    # Müəllif hüquqları mətni
+    "copyright": "Muradov İT",
+    
+
+    # Modellərin göstərilməsi üçün UI tənzimləmələri
+    "show_ui_builder": True,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-dark",
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": "darkly",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
