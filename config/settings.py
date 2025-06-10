e2997811f07ab4ac4c0a7157a9f3a78f72eb2a0b
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-10 19:32:21 +0400

    Add locale middleware and configure multilingual support with Azerbaijani and English languages

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -209,3 +210,18 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-success"
     }
 }
+
+
+
+from django.utils.translation import gettext_lazy as _
+
+# Saytda mövcud olacaq dillər
+LANGUAGES = [
+    ('az', _('Azərbaycan')),
+    ('en', _('English')),
+]
+
+# Tərcümə fayllarının yerləşəcəyi qovluq
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
