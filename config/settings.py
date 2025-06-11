792152e82f299f06c895e1f1ed162dcd0364964e
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-11 12:43:07 +0400

    Add base HTML template with responsive design and dark mode support

--- config/settings.py
+++ config/settings.py
@@ -60,15 +60,15 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages', 
-                'core.context_processors.language_switcher_context',
-                'django.template.context_processors.request', # Dil dəyişdirmə kontekst prosessoru
+                'django.contrib.messages.context_processors.messages',
+                'core.context_processors.language_switcher_context', 
             ],
         },
     },
