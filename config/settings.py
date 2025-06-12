8e58db381fcddab7c59c06694cbe4cde82ac916e
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-12 10:21:31 +0400

    Add Celery configuration to settings.py for task management

--- config/settings.py
+++ config/settings.py
@@ -307,3 +307,15 @@ LOGGING = {
         },
     },
 }
+
+
+# ==============================================================================
+# CELERY KONFİQURASİYASI
+# ==============================================================================
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
