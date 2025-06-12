389381e581037e36792d3db0363c0b46dfb80487
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-12 10:16:28 +0400

    Add logging configuration to settings.py for improved debugging

--- config/settings.py
+++ config/settings.py
@@ -288,3 +288,22 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
