beb43613be9bb2e381fa2b42810495f04784ffa6
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-13 23:23:23 +0400

    Refactor import order in signals.py for improved organization and readability

--- core/signals.py
+++ core/signals.py
@@ -1,10 +1,12 @@
 # core/signals.py
 
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.conf import settings
+
 from .models import Qiymetlendirme
+
 
 @receiver(post_save, sender=Qiymetlendirme)
 def send_notification_on_new_assignment(sender, instance, created, **kwargs):
