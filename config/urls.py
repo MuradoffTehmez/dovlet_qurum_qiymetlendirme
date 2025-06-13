834c74a49668d55ba900a4a96d60403ad3c4d414
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-13 16:16:42 +0400

    Refactor URL configuration for improved clarity and organization; streamline i18n patterns and enhance comments

--- config/urls.py
+++ config/urls.py
@@ -1,44 +1,38 @@
-# config/urls.py
+# config/urls.py - SON VƏ ƏN ETİBARLI VERSİYA
 
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.conf.urls.i18n import i18n_patterns
+
+# Bizim xüsusi Login View-umuzu import edirik
 from core.views import CustomLoginView
 
-# 1. Dil prefiksi TƏLƏB EDƏN URL-ləri ayrıca təyin edirik
-i18n_urlpatterns = i18n_patterns(
-    # Bizim xüsusi login səhifəmizi birinci qeyd edirik
+# Dilə həssas olan URL-ləri bir siyahıya yığırıq
+i18n_urlpatterns = [
     path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    
-    # Sonra Django-nun qalan autentifikasiya URL-lərini əlavə edirik
-    # (logout, password_reset, password_change və s.)
     path('accounts/', include('django.contrib.auth.urls')),
-    
-    # Əsas tətbiqimizin URL-ləri
     path('', include('core.urls')),
-    
-    # Standart dil üçün də /az/ prefiksinin görünməsini təmin edir
-    prefix_default_language=True,
-)
-
-# 2. Əsas urlpatterns siyahısını yaradırıq və dilə həssas olmayanları əlavə edirik
-urlpatterns = [
-    # Dil prefiksi olmayan URL (yalnız admin paneli)
-    path('admin/', admin.site.urls),
 ]
 
-# 3. Dilə həssas URL-ləri əsas siyahıya əlavə edirik
-urlpatterns += i18n_urlpatterns
+# Əsas urlpatterns siyahısını yaradırıq
+urlpatterns = [
+    # Dil prefiksi olmayan admin panelini əlavə edirik
+    path('admin/', admin.site.urls),
+    
+    # Dilə həssas olan URL-ləri i18n_patterns ilə əlavə edirik
+    # prefix_default_language=True standart dil üçün də /az/ prefiksini təmin edir
+    *i18n_patterns(*i18n_urlpatterns, prefix_default_language=True),
+]
 
-# 4. Yalnız DEVELOPMENT rejimində statik və media faylları üçün URL-lər əlavə edirik
+# Development rejimində (DEBUG=True) statik və media fayllarını əlavə edirik
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# 5. Xəta səhifələrini təyin edirik (string formatında daha stabildir)
+
+# Xəta handler-lərini təyin edirik
 handler400 = 'django.views.defaults.bad_request'
 handler403 = 'django.views.defaults.permission_denied'
 handler404 = 'django.views.defaults.page_not_found'
