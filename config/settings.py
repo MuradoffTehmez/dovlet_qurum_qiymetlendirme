5eb8e87262806d842be88e49ceb48ae8a9746010
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-15 16:37:50 +0400

    Disable captcha integration by commenting out the related entry in INSTALLED_APPS.

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "simple_history",
-    'captcha',
+    # 'captcha',
     # Django daxilində olanlar
     "django.contrib.admin",
     "django.contrib.auth",
