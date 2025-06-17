132b21a55bc6535aafdb66248b43d5f0b37e2526
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-17 17:12:24 +0400

    Refactor URL patterns for improved organization and clarity

--- config/urls.py
+++ config/urls.py
@@ -1,34 +1,38 @@
-# config/urls.py - SON VƏ ƏN ETİBARLI VERSİYA
+# config/urls.py
 
-from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-
-# Bizim xüsusi Login View-umuzu import edirik
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from core.views import CustomLoginView
 
-i18n_urlpatterns = [
-    path("accounts/login/", CustomLoginView.as_view(), name="login"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("core.urls")),
-]
+# Dilə həssas olan URL-lər
+i18n_urlpatterns = i18n_patterns(
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('core.urls')),
+    prefix_default_language=True,
+)
 
-# Əsas urlpatterns siyahısını yaradırıq
+# Dilə həssas olmayan URL-lər
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
-
-    *i18n_patterns(*i18n_urlpatterns, prefix_default_language=True),
-
 ]
+urlpatterns += i18n_urlpatterns
+
+# --- YENİ VƏ VACİB HİSSƏ ---
+# Yalnız DEVELOPMENT rejimində (DEBUG=True) statik və media faylları üçün URL-lər
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # `collectstatic` ilə 'staticfiles' qovluğuna yığılan faylları göstərmək üçün
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    
+    # İstifadəçilərin yüklədiyi şəkilləri və faylları 'media' qovluğundan göstərmək üçün
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Xəta handler-lərini təyin edirik
-handler400 = "django.views.defaults.bad_request"
-handler403 = "django.views.defaults.permission_denied"
-handler404 = "django.views.defaults.page_not_found"
-handler500 = "django.views.defaults.server_error"
+
+# Xəta handler-ləri
+handler400 = 'django.views.defaults.bad_request'
+handler403 = 'django.views.defaults.permission_denied'
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
